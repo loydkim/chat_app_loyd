e4440b66d7c0bd24085b2d6e7e6db97b6e3605bb
Loydkim <kimloydmail@gmail.com>
2020-08-14 20:30:25 -0700

    Update const.dart

--- lib/Model/const.dart
+++ lib/Model/const.dart
@@ -1,2 +1,2 @@
-const String firebaseCloudserverToken = 'AAAAFxtLywg:APA91bFbcXfhUI2b2MagqgYnL-2vbJW8IvJ3rN6oKiLQsJJUblIe2Z_mECNImwYo7i-JDmMs2DkF_UXAMET-sGb_czm4JS47yDNKq48iG468rHj8MrXU3eaSRK8ufafS4qeOrsnYou2H';
+const String firebaseCloudserverToken = 'YOUR_FIREBASE_CLOUD_SERVER_TOKEN';//AAAAFxtLywg:APA91bFbcXfhUI2b2MagqgYnL-2vbJW8IvJ3rN6oKiLQsJJUblIe2Z_mECNImwYo7i-JDmMs2DkF_UXAMET-sGb_czm4JS47yDNKq48iG468rHj8MrXU3eaS
 const String youtubeChannelLink = 'https://www.youtube.com/channel/UCLNCErWFQ6LZoaV_JKOq_lQ';
