2ce5b34de4c8003c06440da5ba167816f2fcc8a9
Loydkim <kimloydmail@gmail.com>
2021-03-16 11:11:07 -0700

    Update const.dart

--- lib/Model/const.dart
+++ lib/Model/const.dart
@@ -1,4 +1,4 @@
 // Add your Server token: Go to "Firebase console -> Project setting -> Cloud messaging -> Server key"
-const String firebaseCloudserverToken = 'AAAASD0XwOs:APA91bGcg-RVrDs372DblIr7VvrGUV8XS5mfCNLHgjMXZ51Z6xIagQ5qPZlRfAu2H5ix19DFyhQkBhdTCHBuvLCCcg-diTH8rGAlfwGnfQMYASeJN3Px7fCWvNWGqtd_gqhYt574n76S';//AAAAFxtLywg:APA91bFbcXfhUI2b2MagqgYnL
-const String firebaseCloudvapidKey = 'BBsA_1--B_LqTlXFiv8ArhaNyOhqkLuNM0wAXKMakuuTQUPmNwG4KrzMgV5aCTO31kMX8Zmr0YcSZDpmEmdfkkg';
+const String firebaseCloudserverToken = 'YOUR_FIREBASE_KEY';//AAAAFxtLywg:APA91bFbcXfhUI2b2MagqgYnL
+const String firebaseCloudvapidKey = 'YOUR_VAPID_KEY';
 const String youtubeChannelLink = 'https://www.youtube.com/channel/UCLNCErWFQ6LZoaV_JKOq_lQ';
