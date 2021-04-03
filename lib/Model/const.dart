283ef951ed533804628523711336022cd8550a00
Loydkim <kimloydmail@gmail.com>
2021-04-03 13:38:38 -0700

    Update const.dart

--- lib/Model/const.dart
+++ lib/Model/const.dart
@@ -2,3 +2,9 @@
 const String firebaseCloudserverToken = 'YOUR_FB_SERVER_KEY';//AAAAFxtLywg:APA91bFbcXfhUI2b2MagqgYnL
 const String firebaseCloudvapidKey = 'YOUR_VAPID_KEY';
 const String youtubeChannelLink = 'https://www.youtube.com/channel/UCLNCErWFQ6LZoaV_JKOq_lQ';
+
+const chatInstruction = """Chat App is committed to maintaining a healthy chat, and blocks users who disseminated vegan chats or photos.
+
+We do not provide any other services other than this application. Beware of scam or illegal website promotion.
+
+Attempts to send obscene, offensive, racist messages or request money transactions can result in permanent suspension and criminal prosecution.""";
